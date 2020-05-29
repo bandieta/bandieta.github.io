56dcda68bf776ccd205df7c43c51ee625207622b
bandieta <michal.bandrowski@gmail.com>
2020-05-29 19:21:48 +0200

    build1

---
+++ precache-manifest.f66d7420e30277b8a11da0585042e899.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa9bea5ead04957fb9eda653cff91db3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "658200df1a70e4757d19",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f73927d754f513636451",
+    "url": "/static/js/2.a5e258bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a5e258bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "658200df1a70e4757d19",
+    "url": "/static/js/main.da409993.chunk.js"
+  },
+  {
+    "revision": "2d04db641e9238ce753e",
+    "url": "/static/js/runtime-main.63a59bc4.js"
+  },
+  {
+    "revision": "8e0d2844ea35c76d6b4593388b3c8272",
+    "url": "/static/media/blacha.8e0d2844.gif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
